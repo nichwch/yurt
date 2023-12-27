88a7bcfe788d0c12939e2ba8dc7030b84ac43b3b
nichwch <nichwch@gmail.com>
2023-12-26 21:55:50 -0600

    static adapter!

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,7 @@
 import { getAllEligibleFiles } from '$lib/getAllEligibleFiles';
 import type { LayoutServerLoad } from './$types';
 
+export const prerender = true;
 export const load = (async () => {
     const allFiles = getAllEligibleFiles('./posts');
     console.log(allFiles)
