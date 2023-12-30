1cbfc4aef52ccf57b636a7db10f5f997eb88da72
nichwch <nichwch@gmail.com>
2023-12-30 14:39:07 -0600

    cool cyborg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,11 @@
-<h1>
-	Welcome to my blog! Click on a post in the sidebar to view it. Click on a paragraph to view
-	related ideas.
-</h1>
+<div class="md:w-[26rem] lg:w-[36rem] mx-auto md:p-0 px-5">
+	<h1>
+		Welcome to my blog! Click on a post in the sidebar to view it. Click on a paragraph to view
+		related ideas.
+	</h1>
+	<img
+		class=" w-full border border-black mt-10"
+		src="nomadhypertextopaque.png"
+		alt="cool robot riding horse"
+	/>
+</div>
