6afe20dff8d04ed94242896aafe122fbe8c87e10
nichwch <nichwch@gmail.com>
2023-12-30 23:13:57 -0600

    comment and ~ rendering

--- src/routes/[...pathName]/+page.svelte
+++ src/routes/[...pathName]/+page.svelte
@@ -62,18 +62,30 @@
 		{#if segment.trim().length > 0}
 			<!-- svelte-ignore a11y-click-events-have-key-events -->
 			<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-			<div
-				role="article"
-				id="editor-block-{index}"
-				class="text-left block p-1 break-words md:hover:bg-red-100 transition-colors whitespace-pre-wrap"
-				class:bg-red-100={$focusedText?.trim() === segment?.trim()}
-				on:click={() => {
-					focusedText.set(segment);
-					related.set(pageIndex[segment]);
-				}}
-			>
-				{segment}
-			</div>
+			{#if segment.trim().startsWith('//')}
+				<div
+					role="article"
+					id="editor-block-{index}"
+					class="text-left text-green-800 block p-1 break-words whitespace-pre-wrap"
+				>
+					{segment}
+				</div>
+			{:else if segment.trim() === '~'}
+				<div class="text-gray-300">~</div>
+			{:else}
+				<div
+					role="article"
+					id="editor-block-{index}"
+					class="text-left block p-1 break-words md:hover:bg-red-100 transition-colors whitespace-pre-wrap"
+					class:bg-red-100={$focusedText?.trim() === segment?.trim()}
+					on:click={() => {
+						focusedText.set(segment);
+						related.set(pageIndex[segment]);
+					}}
+				>
+					{segment}
+				</div>
+			{/if}
 		{:else}
 			<br />
 		{/if}
