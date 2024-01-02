5bb7bf2fe5850f171742323a644e7b0ff41a88ca
nichwch <nichwch@gmail.com>
2024-01-01 23:25:51 -0600

    newlines in search results

--- src/routes/SearchEntry.svelte
+++ src/routes/SearchEntry.svelte
@@ -1,13 +1,16 @@
-<script lang='ts'>
-export let entry;
- </script>
-<div class='py-2 w-full'>
-    <div class='text-sm'>
-        <a class='underline' href={'/'+entry.parent+'?search='+entry.content}>{entry.parent.split('/').pop()} </a>
-        <span class='text-red-700'>[{Math.floor(entry.score * 100)}% similar]</span>
-    </div>
+<script lang="ts">
+	export let entry;
+</script>
 
-    <div class='w-full break-words'>
-        {entry.content}
-    </div>
+<div class="py-2 w-full">
+	<div class="text-sm">
+		<a class="underline" href={'/' + entry.parent + '?search=' + entry.content}
+			>{entry.parent.split('/').pop()}
+		</a>
+		<span class="text-red-700">[{Math.floor(entry.score * 100)}% similar]</span>
+	</div>
+
+	<div class="w-full break-words whitespace-pre-wrap">
+		{entry.content}
+	</div>
 </div>
