43303ae235763ba686dc281ab016c967c6127d12
nichwch <nichwch@gmail.com>
2024-01-03 12:26:36 -0500

    padding so text lines up with titles

--- src/routes/[...pathName]/+page.svelte
+++ src/routes/[...pathName]/+page.svelte
@@ -54,7 +54,7 @@
 
 <title>{postName}</title>
 <div class="md:w-[26rem] lg:w-[36rem] mx-auto md:p-0 px-5">
-	<div class="mb-5">
+	<div class="mb-5 px-1">
 		<h1 class="text-lg font-semibold">{postName}</h1>
 		<div class="mb-2">
 			<i class="mr-3">{publishDate.toDateString()} </i>
